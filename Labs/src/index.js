dc857df19be9d30e34c61da133d150f97b0716d5
vvo0002 <vvo002@mx.wal-mart.com>
2018-08-20 16:49:05 -0500

    Adding linter

--- Labs/src/index.js
+++ Labs/src/index.js
@@ -1,19 +1,19 @@
-alert("Hello World");
-document.getElementById("main").innerHTML = "Hello world";
+alert('Hello World');
+document.getElementById('main').innerHTML = 'Hello world';
 
 
 function showRandomWords() {
-    let i = 1;
+    let counter = 1;
     let randomNumber = getRandomNumber();
     /*for (let i = 1; i <= getRandomNumber(); i++) {
-        let newElement = document.createElement("div");
+        let newElement = document.createElement('div');
         newElement.innerHTML = getRhyme(i);
-        document.getElementById("main").append(newElement);
+        document.getElementById('main').append(newElement);
     }*/
-    while(i <= randomNumber) {
-        let newElement = document.createElement("div");
-        newElement.innerHTML = getRhyme(i++);
-        document.getElementById("main").append(newElement);
+    while(counter <= randomNumber) {
+        let newElement = document.createElement('div');
+        newElement.innerHTML = getRhyme(counter++);
+        document.getElementById('main').append(newElement);
     }
 
 }
@@ -29,26 +29,26 @@ function getRandomNumber() {
 
 function getRhyme(number) {
     switch (number) {
-        case 1:
-            return "fun";
-        case 2:
-            return "blue";
-        case 3:
-            return "free";
-        case 4:
-            return "pour";
-        case 5:
-            return "hive";
-        case 6:
-            return "kicks";
-        case 7:
-            return "heaven";
-        case 8:
-            return "skate";
-        case 9:
-            return "rhyme";
-        case 10:
-            return "pen";
+    case 1:
+        return 'fun';
+    case 2:
+        return 'blue';
+    case 3:
+        return 'free';
+    case 4:
+        return 'pour';
+    case 5:
+        return 'hive';
+    case 6:
+        return 'kicks';
+    case 7:
+        return 'heaven';
+    case 8:
+        return 'skate';
+    case 9:
+        return 'rhyme';
+    case 10:
+        return 'pen';
     }
 }
 
