678eeaf5780de888179844c2cefc4f60de60a0ea
vvo0002 <vvo002@mx.wal-mart.com>
2018-08-22 10:23:41 -0500

    Adding tests

--- Labs/spec/romanNumeralConverter.spec.js
+++ Labs/spec/romanNumeralConverter.spec.js
@@ -1,8 +1,47 @@
 import convert from '../src/romanNumeralConverter.js';
 
 describe('Roman numeral converter', () => {
-    it('will return 1 given i', () => {
-        let actual = convert('i');
-        expect(actual).toEqual(1);
+    it('Will return I given 1', () => {
+        let actual = convert(1);
+        expect(actual).toEqual('I');
+    }); 
+
+    it('Will return VI given 6', () => {
+        let actual = convert(6);
+        expect(actual).toEqual('VI');
+    });
+
+    it('Will return XXXIV given 34', () => {
+        let actual = convert(34);
+        expect(actual).toEqual('XXXIV');
+    });
+
+    it('Will return XLIX given 49', () => {
+        let actual = convert(49);
+        expect(actual).toEqual('XLIX');
+    });
+
+    it('Will return CDLXXVIII given 478', () => {
+        let actual = convert(478);
+        expect(actual).toEqual('CDLXXVIII');
+    });
+
+    it('Will return MCMXC given 1990', () => {
+        let actual = convert(1990);
+        expect(actual).toEqual('MCMXC');
+    });
+
+    it('Will return MMXVI given 2016', () => {
+        let actual = convert(2016);
+        expect(actual).toEqual('MMXVI');
+    });
+
+    it('Will return " " given 0', () => {
+        let actual = convert(0);
+        expect(actual).toEqual('');
+    });
+
+    it('Will throw an exception',  () => {
+        expect(() => convert(3500)).toThrowError(TypeError);
     });
 });
--- Labs/src/romanNumeralConverter.js
+++ Labs/src/romanNumeralConverter.js
@@ -1,8 +1,17 @@
-function convert(romanNumeral) { // eslint-disable-line no-unused-vars
-    if(romanNumeral === 'i') {
-        return 1;
-    } else {
-        throw 'Not implemented yet';
+function convert(num) {
+    var roman = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
+    var result = '';
+
+    if(num > 3000 || num < 0) {
+        throw new TypeError('a');
     }
+
+    for (var i in roman) {
+        while (num >= roman[i]) {
+            result += i;
+            num -= roman[i];
+        }
+    }
+    return result;
 }
-export default convert;
+export default convert; 
