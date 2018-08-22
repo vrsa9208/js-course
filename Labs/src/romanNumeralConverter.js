7e36d18951b6b9afcc29737be7fae2b881dcd1f1
vvo0002 <vvo002@mx.wal-mart.com>
2018-08-22 09:18:06 -0500

    Adding jasmine report

--- Labs/src/romanNumeralConverter.js
+++ Labs/src/romanNumeralConverter.js
@@ -1,4 +1,8 @@
 function convert(romanNumeral) { // eslint-disable-line no-unused-vars
-    throw 'Not yet implemented';
+    if(romanNumeral === 'i') {
+        return 1;
+    } else {
+        throw 'Not implemented yet';
+    }
 }
 export default convert;
