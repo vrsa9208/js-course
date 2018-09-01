2897516afa3c7b697c0c1893fb0081d7bc47bb81
vrsa9208 <vrsa9208@gmail.com>
2018-09-01 10:59:09 -0500

    Lab 14 bonus: AJAX in node

--- Labs/src/server.js
+++ Labs/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/marco', function (req, res) {
     return res.send('polo');
+});
+
+app.get('/people', function (req, res) {
+    var quantity = req.query.quantity || 5;
+    fetch(`https://randomuser.me/api/?results=${quantity}`)
+    .then(response => response.json())
+    .then(jsonResponse => res.send(jsonResponse.results));
 });
 app.use(express.static(path.join(__dirname, '../dist')));
 app.listen(port, () => console.log(`Listening on port ${port}`));
